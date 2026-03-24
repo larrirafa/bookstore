80608c2fcf9625d5aba8193a0b1c8c39f6ba96e3
larrirafa <larrirafael2008@gmail.com>
2026-03-23 23:07:24 -0300

    fix staticfiles storage

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -94,7 +94,7 @@ USE_TZ = True
 # STATIC FILES
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 # REST FRAMEWORK
 REST_FRAMEWORK = {
