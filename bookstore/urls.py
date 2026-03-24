b766d673640f8c6ba69d59c0e1454538f248a8f6
larrirafa <larrirafael2008@gmail.com>
2026-03-23 22:49:30 -0300

    fix urls

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -1,38 +1,26 @@
-"""
-URL configuration for bookstore project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.http import HttpResponse
-import debug_toolbar
 from django.contrib import admin
 from django.urls import path, re_path, include
+from django.http import HttpResponse
 from rest_framework.authtoken.views import obtain_auth_token
+import debug_toolbar
 
 
-def home(request):
-    return HttpResponse("API Bookstore rodando 🚀")
+# 🔹 View simples para testar a API
+def api_root(request, version):
+    return HttpResponse(f"API Bookstore {version} rodando 🚀")
 
 
 urlpatterns = [
-    path("", home),  # 👈 ADICIONA ISSO
-
     path("__debug__/", include(debug_toolbar.urls)),
     path("admin/", admin.site.urls),
 
-    re_path("bookstore/(?P<version>(v1|v2))/", include("order.urls")),
-    re_path("bookstore/(?P<version>(v1|v2))/", include("product.urls")),
+    # ✅ ROTA BASE
+    re_path(r"^bookstore/(?P<version>(v1|v2))/$", api_root),
 
+    # ✅ ROTAS ORGANIZADAS
+    re_path(r"^bookstore/(?P<version>(v1|v2))/orders/", include("order.urls")),
+    re_path(r"^bookstore/(?P<version>(v1|v2))/products/", include("product.urls")),
+
+    # 🔐 TOKEN
     path("api-token-auth/", obtain_auth_token, name="api_token_auth"),
 ]
