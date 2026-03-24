ab78cf44d4b3a52a5ea2cf35149441adc86774c9
larrirafa <larrirafael2008@gmail.com>
2026-03-23 22:36:42 -0300

    feat: add home route

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -14,16 +14,25 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.http import HttpResponse
 import debug_toolbar
 from django.contrib import admin
 from django.urls import path, re_path, include
 from rest_framework.authtoken.views import obtain_auth_token
 
+
+def home(request):
+    return HttpResponse("API Bookstore rodando 🚀")
+
+
 urlpatterns = [
+    path("", home),  # 👈 ADICIONA ISSO
+
     path("__debug__/", include(debug_toolbar.urls)),
     path("admin/", admin.site.urls),
+
     re_path("bookstore/(?P<version>(v1|v2))/", include("order.urls")),
     re_path("bookstore/(?P<version>(v1|v2))/", include("product.urls")),
+
     path("api-token-auth/", obtain_auth_token, name="api_token_auth"),
 ]
